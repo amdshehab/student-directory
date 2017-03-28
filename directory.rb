c71ae4608f2d7193b790c9bba6c851342271c92c
Ahmed <shehab.ahmed.uk@gmail.com>
2017-03-28 19:27:28 +0000

    code refactoring

--- directory.rb
+++ directory.rb
@@ -1,16 +1,19 @@
-student_count = 11
-puts "The students of Villains Academy"
-puts "------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#finally we print of our list of students
-print "Overall, we have #{student_count} great students"
+# let's put all students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+puts "The students of Villains academy"
+puts "---------------------"    
+
+puts students[0..10]
+puts "Overall, we have #{students.count} great students"
