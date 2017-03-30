60e45abd04596779a4fe3269f5e46888cbf617c3
Ahmed <shehab.ahmed.uk@gmail.com>
2017-03-30 14:40:22 +0000

    commiting my directory upto step 8 ex.6

--- directory.rb
+++ directory.rb
@@ -1,50 +1,45 @@
 def input_students
-  puts "please enter the names of the students"
-  puts "to finish, just hit return twice"
+  puts "please enter the names of the student".center(150)
   #creating an empty array
   students = []
   #getting the first name
   name = gets.chomp
   #while the name is not empty; repeat this code
-  while !name.empty? do
+  puts "please enter the student's country of origin !".center(150)
+  country = gets.chomp
+while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end
-  #return the arrays of students
-  students
+    students << {name: name, country: country, cohort: :november}
+    puts "Now we have #{students.count} students".center(150)
+    puts "would you like to add another student? answer with yes or no".center(150)
+    answer = gets.chomp
+    if answer == "yes"
+      puts "student name? Remember you can cancel by hitting enter twice !".center(150)
+      name = gets.chomp
+      puts "country of origin?".center(150)
+      country = gets.chomp
+    elsif answer == "no"
+      break
+     end
+end
+ students
 end
  
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+
 
 def print_header
-puts "The students of Villains academy"
-puts "---------------------"  
+puts "The students of Villains academy".center(150)
+puts "---------------------".center(150)  
 end
 
 def print(students)
 students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts "#{student[:name]} from #{student[:country]} (#{student[:cohort]} cohort)".center(150)
 end
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+puts "Overall, we have #{names.count} great students".center(150)
 end
 
  students = input_students
