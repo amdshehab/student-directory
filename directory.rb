a2904ae347e964fbd6faa7d960a791dfa333a7de
Ahmed <shehab.ahmed.uk@gmail.com>
2017-04-01 19:19:31 +0000

    Commiting a fully operational Cmnd/line directory

--- directory.rb
+++ directory.rb
@@ -1,8 +1,9 @@
 @students = []
+
 def interactive_menu
     loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
 end
 end
 
@@ -10,17 +11,15 @@ def input_students
   puts "please enter the names of the student".center(150)
   #creating an empty array
   #getting the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty; repeat this code
- 
-  
   while !name.empty? do
      @students = []
     #add the student hash to the array
      puts "please enter the student's country of origin !".center(150)
-     country = gets.chomp
+     country = STDIN.gets.chomp
      puts "please input the students cohort".center(150)
-     cohort = gets.chomp
+     cohort = STDIN.gets.chomp
      require 'date'
     if Date::MONTHNAMES.include?(cohort&&cohort.capitalize)
      cohort.to_sym
@@ -28,13 +27,13 @@ def input_students
      puts "that is an invalid date, we have automaticlly selected the nearest cohort available".center(150)
      cohort = :november
     end
-    @students << {name: name, country: country, cohort: cohort}
+   @students << {name: name, country: country, cohort: cohort.to_sym}
     puts "Now we have #{@students.count} students".center(150)
     puts "would you like to add another student? answer with yes or no".center(150)
-    answer = gets.chomp
+    answer = STDIN.gets.chomp
     if answer == "yes"
     puts "what's the next students name?".center(150)
-    name = gets.chomp
+    name = STDIN.gets.chomp
     elsif answer == "no"
       break
     end
@@ -68,6 +67,7 @@ def print_menu
    puts "1. Input the students"
    puts "2. Show the students"
    puts "3. Save the list to students.csv"
+   puts "4. load the list ffrom students.csv"
    puts "9. Exit" 
 end
 
@@ -85,6 +85,8 @@ def process(selection)
         show_students
     when "3"
         save_students
+    when "4"
+        load_students
     when "9"
         exit
     else
@@ -102,18 +104,32 @@ def save_students
     end
     file.close
 end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
+def load_students(filename = "students.csv")
+    file = File.open("students.csv","r")
+    file.readlines.each do |line|
+     name, country, cohort = line.chomp.split(",")
+    @students << {name: name, country: country, cohort: cohort.to_sym}
+  end
+ file.close
+end
+
+def try_load_students
+    filename = ARGV.first
+    if filename.nil?
+        load_students(filename = "students.csv")
+        puts "loaded #{@students.count} from #{filename}"
+    elsif File.exists?(filename)
+        load_students(filename)
+         puts "loaded #{@students.count} from #{filename}"
+     else
+         puts "sorry, #{filename} does not exist."
+         exit
+     end
+ end
  
+ 
+ try_load_students
  interactive_menu
  
   
